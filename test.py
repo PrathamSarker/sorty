4c79987c6d366c590adfa1bdd588dedc3123d85a
Pratham Sarker <233599776+PrathamSarker@users.noreply.github.com>
2026-05-17 00:14:44 -0400

    Implement course folder creation feature
    Added functionality to create folders based on user input for course names.

--- test.py
+++ test.py
@@ -6,5 +6,30 @@ source_folder = input("Enter a valid Path for source folder: ")       #input pat
 
 source_folder = Path(source_folder)     #converting string to path object
 
-for file_name in source_folder.iterdir():
-    print(file_name.name)
+
+course_list = []
+course_name = ""
+
+while(course_name != 'done'):
+    course_name = input("Enter course name (Enter 'done' to finish): ")
+    if course_name == 'done':
+        break
+    course_list.append(course_name)
+
+print(f"These are all the courses: {course_list}")
+
+
+for names in course_list:
+    os.makedirs(names, exist_ok = True)
+    print(f"folder created: {names}")
+
+
+
+
+
+
+
+
+#for file in source_folder.iterdir():
+#    print(file.name)
+
